a671ceea6a7151f22feb30f9ac1b272e1d224beb
SeongGyeongOh <kamniang@naver.com>
2020-12-22 18:07:07 +0900

    backpress...

--- app/src/main/java/com/practice/practicesharedelement/MainActivity.kt
+++ app/src/main/java/com/practice/practicesharedelement/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.practice.practicesharedelement
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.Crossfade
@@ -21,11 +22,15 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.*
+import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.AmbientDensity
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.lifecycle.ViewModelProvider
 import com.practice.practicesharedelement.navigation.Actions
 import com.practice.practicesharedelement.navigation.AmbientBackDispatcher
 import com.practice.practicesharedelement.navigation.Destination
@@ -33,15 +38,18 @@ import com.practice.practicesharedelement.navigation.Navigator
 import com.practice.practicesharedelement.ui.PracticeSharedElementTheme
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         setContent {
             PracticeSharedElementTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     RootScreen(
-                            backDispatcher = onBackPressedDispatcher
+                            backDispatcher = onBackPressedDispatcher,
+                            mainViewModel
                     )
                 }
             }
@@ -50,7 +58,10 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun RootScreen(backDispatcher: OnBackPressedDispatcher) {
+fun RootScreen(
+        backDispatcher: OnBackPressedDispatcher,
+        mainViewModel: MainViewModel
+) {
     val navigator: Navigator<Destination> = rememberSavedInstanceState(
         saver = Navigator.saver(backDispatcher)
     ) {
@@ -64,13 +75,15 @@ fun RootScreen(backDispatcher: OnBackPressedDispatcher) {
                 when(destination) {
                     is Destination.FirstScreen -> {
                         Screen1(
-                            moveToNextScreen = actions.goToSecond
+                            moveToNextScreen = actions.goToSecond,
+                            mainViewModel
                         )
                     }
                     is Destination.SecondScreen -> {
                         Screen2(
                                 destination,
-                                backDispatcher
+                                backDispatcher,
+                                mainViewModel
                         )
                     }
                 }
@@ -81,15 +94,17 @@ fun RootScreen(backDispatcher: OnBackPressedDispatcher) {
 
 @Composable
 fun Screen1(
-        moveToNextScreen: (item: User, offset: Offset, sizeList: List<Dp>) -> Unit
+        moveToNextScreen: (item: User, offset: Offset, sizeList: List<Dp>) -> Unit,
+        mainViewModel: MainViewModel
 ) {
     Column(Modifier.fillMaxSize()) {
         WithConstraints() {
             LazyColumn(Modifier.fillMaxWidth()) {
-                items(tempList) { item ->
-                    ListItem(
-                        moveToNextScreen = moveToNextScreen,
-                        item = item,
+                itemsIndexed(tempList) { index, item ->
+                    ListItem(moveToNextScreen = moveToNextScreen,
+                            item = item,
+                            itemIndex = index,
+                            mainViewModel = mainViewModel
                     )
                 }
             }
@@ -101,6 +116,7 @@ fun Screen1(
 fun Screen2(
         item: Destination.SecondScreen,
         backDispatcher: OnBackPressedDispatcher,
+        mainViewModel: MainViewModel,
 ) {
     WithConstraints {
         val screenState = remember { mutableStateOf(false) }
@@ -109,77 +125,136 @@ fun Screen2(
         val width = remember { mutableStateOf(item.list[0])}
         val height = remember { mutableStateOf(item.list[1])}
         val alpha = remember { mutableStateOf(0f)}
+
         if(screenState.value) {
-            xOffset.value = 0f
+            xOffset.value = 50f
             yOffset.value = 0f
             width.value = with(AmbientDensity.current) {constraints.maxWidth.toDp()}
             height.value = 300.dp
             alpha.value = 1f
+
+            mainViewModel.xOffset.value = xOffset.value
+            mainViewModel.yOffset.value = yOffset.value
+            mainViewModel.width.value = width.value
+            mainViewModel.height.value = height.value
         }
 
         onActive(callback = {
             screenState.value = true
         })
 
-        backDispatcher.onBackPressed(
-
-        )
-
-
         Box(
             Modifier.fillMaxSize()
                     .background(Color.Gray),
         ) {
             Image(
-                imageResource(id = item.user.img),
-                Modifier.preferredWidth(animate(target = width.value, animSpec = tween(1000)))
-                        .preferredHeight(animate(target = height.value, animSpec = tween(1000)))
-                        .offset(
-                                x = animate(
-                                        target = xOffset.value.dp,
-                                        animSpec = tween(1000)
-                                ),
-                                y = animate(
-                                        target = yOffset.value.dp,
-                                        animSpec = tween(1000)
-                                )
-                        )
-                        .alpha(animate(target = alpha.value, animSpec = tween(1000)))
+                    imageResource(id = item.user.img),
+                    Modifier.preferredWidth(animate(target = width.value, animSpec = tween(1000)))
+                            .preferredHeight(animate(target = height.value, animSpec = tween(1000)))
+                            .offset (
+                                    x = animate(
+                                            target = xOffset.value.dp,
+                                            animSpec = tween(1000)
+                                    ),
+                                    y = animate(
+                                            target = yOffset.value.dp,
+                                            animSpec = tween(1000)
+                                    )
+                            )
+                            .alpha(animate(target = alpha.value, animSpec = tween(1000)))
+                            .onGloballyPositioned {
+                                mainViewModel.xOffset.value = it.boundsInParent.topLeft.x
+                                mainViewModel.yOffset.value = it.boundsInParent.topLeft.y
+                            }
+                            .clickable(onClick = { Log.d("asdf", "${mainViewModel.xOffset.value} : ${mainViewModel.yOffset.value}") })
             )
             Text(item.user.name)
         }
     }
 }
 
-
 @Composable
 fun ListItem(
         moveToNextScreen: (item: User, offset: Offset, sizeList: List<Dp>) -> Unit,
         item: User,
+        mainViewModel: MainViewModel,
+        itemIndex: Int,
 ) {
     WithConstraints {
-        val height = 70.dp
-        val width = 70.dp
-
+        val screenState = remember { mutableStateOf(false) }
+        val fixedWidth = 70.dp
+        val fixedHeight = 70.dp
+        val width = remember { mutableStateOf(fixedWidth) }
+        val height = remember { mutableStateOf(fixedHeight) }
+        val xOffset = remember { mutableStateOf(0f) }
+        val yOffset = remember { mutableStateOf(0f) }
         val offset = remember { mutableStateOf(Offset(0))}
 
+        mainViewModel.height.observeForever{
+            height.value = it
+        }
+        mainViewModel.width.observeForever{
+            width.value = it
+        }
+        mainViewModel.xOffset.observeForever{
+            xOffset.value = it
+        }
+        mainViewModel.yOffset.observeForever{
+            yOffset.value = it
+        }
+
+        if(screenState.value) {
+            width.value = fixedWidth
+            height.value = fixedHeight
+            xOffset.value = 0f
+            yOffset.value = 0f
+        }
+
+        onActive(callback = {
+            screenState.value = true
+        })
+
         Box(Modifier.fillMaxSize()) {
-            Row(
-                Modifier.fillMaxWidth()
-                        .preferredHeight(height = height)
-                        .preferredWidth(width)
-                        .clickable(onClick = {
-                            moveToNextScreen(item, offset.value, listOf(height, width))
-                        })
-            )
-            {
-                Image(
-                    imageResource(id = item.img),
-                    Modifier.preferredSize(50.dp)
-                            .onGloballyPositioned {
+            Row() {
+                if(itemIndex == mainViewModel.index.value) {
+                    Image(
+                            imageResource(id = item.img),
+                            Modifier.onGloballyPositioned {
                                 offset.value = it.positionInRoot.div(3f)
                             }
-                )
+                                    .preferredWidth(animate(target = width.value, animSpec = tween(3000)))
+                                    .preferredHeight(animate(target = height.value, animSpec = tween(3000)))
+                                    .offset(
+                                            x = animate(target = xOffset.value.dp, animSpec = tween(3000)),
+                                            y = animate(target = yOffset.value.dp, animSpec = tween(3000))
+                                    )
+                                    .clickable(onClick = {
+                                        moveToNextScreen(item, offset.value, listOf(height.value, width.value))
+                                        mainViewModel.xOffset.value = offset.value.x
+                                        mainViewModel.yOffset.value = offset.value.y
+                                        mainViewModel.width.value = width.value
+                                        mainViewModel.height.value = height.value
+                                        mainViewModel.index.value = itemIndex
+                                    })
+                    )
+                } else {
+                    Image(
+                            imageResource(id = item.img),
+                            Modifier.onGloballyPositioned {
+                                offset.value = it.globalPosition.div(3f)
+                            }
+                                    .preferredWidth(fixedWidth)
+                                    .preferredHeight(fixedHeight)
+                                    .clickable(onClick = {
+                                        moveToNextScreen(item, offset.value, listOf(height.value, width.value))
+                                        mainViewModel.xOffset.value = offset.value.x
+                                        mainViewModel.yOffset.value = offset.value.y
+                                        mainViewModel.width.value = width.value
+                                        mainViewModel.height.value = height.value
+                                        mainViewModel.index.value = itemIndex
+                                    })
+                    )
+                }
                 Text(item.name)
             }
         }
--- app/src/main/java/com/practice/practicesharedelement/MainViewModel.kt
+++ app/src/main/java/com/practice/practicesharedelement/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.practice.practicesharedelement
 
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.internal.FlowLayout
 
 class MainViewModel: ViewModel() {
-    init {
-
-    }
+    val xOffset = MutableLiveData<Float>()
+    val yOffset = MutableLiveData<Float>()
+    val width = MutableLiveData<Dp>()
+    val height = MutableLiveData<Dp>()
+    val index = MutableLiveData<Int>()
 }
